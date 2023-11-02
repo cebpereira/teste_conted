a30ff1681dd9ebc79ffb95d5541a810af5f514d4
Carlos Elandro <carlos.wx290@gmail.com>
2023-11-02 09:40:44 -0300

    feat: ajustes nas views register, login e footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,7 @@
-<footer class="footer bg-dark relative-bottom d-flex justify-content-center" style="height: 50px">
-    <div class="py-3">
-        <p class="text-white">COPYRIGHT © CARLOS ELANDRO - 2023</p>
+<footer class="footer bg-dark relative-bottom d-flex justify-content-center">
+    <div class="row pt-3"
+        <div class="p-2">
+            <p class="text-white">COPYRIGHT © CARLOS ELANDRO - 2023</p>
+        </div>
     </div>
 </footer>
